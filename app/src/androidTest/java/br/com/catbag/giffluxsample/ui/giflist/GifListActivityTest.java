d89b0c9e3c49fcd7856a026a730a7309929e5140
Nilton Vasques <nilton.vasques@openmailbox.org>
2016-10-15 00:20:17 -0300

    * Mock filedownloader to simulate download failed

--- app/src/androidTest/java/br/com/catbag/giffluxsample/ui/giflist/GifListActivityTest.java
+++ app/src/androidTest/java/br/com/catbag/giffluxsample/ui/giflist/GifListActivityTest.java
@@ -13,6 +13,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import br.com.catbag.giffluxsample.R;
+import br.com.catbag.giffluxsample.actions.GifActionCreator;
+import br.com.catbag.giffluxsample.asyncs.restservice.FileDownloader;
 import br.com.catbag.giffluxsample.ui.GifListActivity;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -20,8 +22,15 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static br.com.catbag.giffluxsample.matchers.Matchers.withBGColor;
+import static br.com.catbag.giffluxsample.matchers.Matchers.withToast;
 import static org.hamcrest.Matchers.not;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -90,6 +99,50 @@ public class GifListActivityTest {
         assert(!mActivityTestRule.getActivity().getGlideWrapper().getResource().isRunning());
     }
 
+    private FileDownloader.FailureDownloadListener listener;
+    private FileDownloader downloader;
+    public void mockFileDownloaderToAlwaysFail(){
+        downloader = mock(FileDownloader.class);
+        doAnswer( (invocation) -> {
+            System.out.println("MOCKED METHOD");
+            listener.onFailure(new Exception("Download error"));
+            return null;
+        }).when(downloader).download(anyString(), anyString());
+        doAnswer( (invocation) -> {
+            Object[] args = invocation.getArguments();
+            Object mock = invocation.getMock();
+            System.out.println("MOCKED FAILURE");
+            listener = (FileDownloader.FailureDownloadListener)args[0];
+            return mock;
+        }).when(downloader).onFailure(any(FileDownloader.FailureDownloadListener.class));
+        doReturn(downloader).when(downloader).onSuccess(any(FileDownloader.SuccessDownloadListener.class));
+        doReturn(downloader).when(downloader).onStarted(any(FileDownloader.StartDownloadListener.class));
+
+        GifActionCreator.getInstance().setFileDownloader(downloader);
+    }
+
+//    @Test
+//    public void gifDownloadFailedTest() {
+//        mockFileDownloaderToAlwaysFail();
+//
+//        mActivityTestRule.launchActivity(new Intent());
+//
+//        onView(withText("Download error")).inRoot(withToast())
+//                .check(matches(isDisplayed()));
+//    }
+
+    private void waitSpinnerGone() {
+        try {
+            GifListActivity activity = mActivityTestRule.getActivity();
+            boolean spinner = activity.findViewById(R.id.loading).getVisibility() == View.VISIBLE;
+            while(spinner){
+                spinner = activity.findViewById(R.id.loading).getVisibility() == View.VISIBLE;
+                Thread.sleep(100);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
     private void waitLoadGif() {
         try {
             GifListActivity activity = mActivityTestRule.getActivity();
