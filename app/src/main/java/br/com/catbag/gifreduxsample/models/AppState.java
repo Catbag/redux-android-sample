f46a6ea59994a75ffd6b9eb8567da8657ebe4c46
Nilton Vasques <nilton.vasques@openmailbox.org>
2016-10-24 18:38:31 -0300

    * Show host IP

--- app/src/main/java/br/com/catbag/gifreduxsample/models/AppState.java
+++ app/src/main/java/br/com/catbag/gifreduxsample/models/AppState.java
@@ -22,7 +22,7 @@ public abstract class AppState {
 
     @Value.Default
     public String getGifUrl() {
-        return "http://127.0.0.1:8000/goku.gif";
+        return "http://10.0.2.2:8000/goku.gif";
     }
 
     @Value.Default
