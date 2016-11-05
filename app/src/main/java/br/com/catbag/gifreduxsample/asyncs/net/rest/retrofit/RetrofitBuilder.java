9048ca7250a755e88ad62ea411f61c99869f0bd2
Nilton Vasques <nilton.vasques@openmailbox.org>
2016-11-05 03:10:00 -0300

    Create download endpoint with a default url
    * Move download route to retrofit builder
    * Create download endpoint route using a default url
    
    Fixes #48

--- app/src/main/java/br/com/catbag/gifreduxsample/asyncs/net/rest/retrofit/RetrofitBuilder.java
+++ app/src/main/java/br/com/catbag/gifreduxsample/asyncs/net/rest/retrofit/RetrofitBuilder.java
@@ -6,14 +6,20 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 import okhttp3.OkHttpClient;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Streaming;
+import retrofit2.http.Url;
 
 /**
  * Created by niltonvasques on 11/1/16.
  */
 
 public final class RetrofitBuilder {
+    private static final String DEFAULT_URL = "http://none.com";
     private static RetrofitBuilder sInstance;
     private Executor mThreadPoolExecutor;
 
@@ -32,6 +38,10 @@ public final class RetrofitBuilder {
         return buildRetrofit(endpoint).create(routes);
     }
 
+    public DownloadRoute createDownloadEndpoint() {
+        return buildRetrofit(DEFAULT_URL).create(DownloadRoute.class);
+    }
+
     private void createExecutor() {
         int cpuCount = Runtime.getRuntime().availableProcessors();
         int corePoolSize = cpuCount + 1;
@@ -45,5 +55,11 @@ public final class RetrofitBuilder {
                 .baseUrl(endpoint)
                 .callbackExecutor(mThreadPoolExecutor)
                 .client(new OkHttpClient()).build();
+    }
+
+    public interface DownloadRoute {
+        @GET
+        @Streaming
+        Call<ResponseBody> downloadFileWithDynamicUrlSync(@Url String fileUrl);
     }
 }
