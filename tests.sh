d57343e4308879b78c20b02b8f1bd0da57aa8148
Nilton Vasques <niltonvasques@users.noreply.github.com>
2016-11-14 12:41:20 -0300

    Capture tests artifacts and publish to gh-pages (#59)
    * Using a recursive and theirs strategy to keep gh-pages history
    * Move tests commands from drone.yml to tests.sh
    * Capture logcat log and upload to gh-pages
    * Setup git user before upload artifacts
    * Setup tests to push artifacts using github access token
    * Add drone secrets to hold sensible information
    * Ignore secrets
    
    Fixes #58

---
+++ tests.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+BUILD_STATUS=0
+
+start_emulator && wait_emulator 
+
+./gradlew check --daemon -PdisablePreDex --stacktrace
+UNIT_STATUS=$?
+
+./gradlew assembleDebugAndroidTest --daemon -PdisablePreDex --stacktrace
+
+unlock_emulator
+
+# Capture logs from emulator
+adb logcat -c
+adb logcat > logcat.log &
+LOGCAT_PID=$!
+
+# Running integration tests
+./gradlew cC -i --daemon -PdisablePreDex --stacktrace
+INTEGRATION_STATUS=$?
+
+kill $LOGCAT_PID
+
+# Uploading artifacts to github pages
+git remote add upstream "https://$GITHUB_ACCESS_TOKEN@github.com/Catbag/redux-android-sample.git"
+git checkout --orphan gh-pages
+git reset HEAD -- .
+git add app/build/reports/ -f
+git add logcat.log -f
+git config --global user.name "Drone CI"
+git config --global user.email "developer@catbag.com.br"
+git commit -am "Publish results from test #$DRONE_BUILD_NUMBER"
+git pull -s recursive -X theirs origin gh-pages --rebase
+git push -f upstream gh-pages
+
+# Computing build status
+BUILD_STATUS=$((UNIT_STATUS + INTEGRATION_STATUS))
+
+# Returning build status
+exit $BUILD_STATUS
